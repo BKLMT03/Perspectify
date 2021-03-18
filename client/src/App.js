f168a96392b4f54ad204c289d1d5cafba04e5a39
Torontow <mtorontow0@gmail.com>
2021-03-18 16:25:03 -0400

    Incorporated components into app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,36 @@
-import './App.css';
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Trending from './components/Trending'
+import Logo from './components/Logo'
+import Container from './components/Container'
+import Col from './components/Col'
+import Navbar from './components/Navbar'
+import Row from './components/Row'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <Header />
-      <Trending />
+    <div className='App'>
+      <Logo />
+      <Router>
+        <Container>
+          <Header />
+          <Row>
+            <Col size='1'>
+              <Navbar />
+            </Col>
+            <Col size='10'>
+              <Route exact path='/' component={Trending} />
+              <Route exact path='/home' component={Trending} />
+              {/* <Route exact path='/savedarticles' component={SavedArticles} /> */}
+            </Col>
+          </Row>
+          <Trending />
+        </Container>
+      </Router>
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
