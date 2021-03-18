68f86b2b8dcdac2eeb0a603ee8df5bbe2fb8614f
Torontow <mtorontow0@gmail.com>
2021-03-18 16:24:16 -0400

    Built Navbar component and associated css file.

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
+
+function Navbar () {
+  return (
+    <nav className='nav flex-columnn link-dark justify-content-center'>
+      <li className='nav-item'>
+        <Link
+          className='nav-link'
+          to='/greatesthits'
+          style={{ color: 'darkslategray', textShadow: '1px 1px black' }}
+        >
+          Home
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link
+          className='nav-link'
+          to='contact'
+          style={{ color: 'darkslategray', textShadow: '1px 1px black' }}
+        >
+          Saved Articles
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link
+          className='nav-link'
+          to='/'
+          style={{ color: 'darkslategray', textShadow: '1px 1px black' }}
+        >
+          LOG IN
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link
+          className='nav-link'
+          to='/'
+          style={{ color: 'darkslategray', textShadow: '1px 1px black' }}
+        >
+          SIGN UP
+        </Link>
+      </li>
+
+    </nav>
+  )
+}
+
+export default Navbar
