5ccc69eaca417e4b3c901f906b39608808d6dfbf
Torontow <mtorontow0@gmail.com>
2021-03-18 21:08:17 -0400

    CSS alterations.

--- client/src/components/Trending/index.js
+++ client/src/components/Trending/index.js
@@ -1,14 +1,15 @@
+import { gray } from 'colors'
 import React from 'react'
 import Article from "../Article"
 // import Topic from '../Topic'
 import Row from '../Row'
 import "./style.css"
 
-const Trending = () => {
+const Trending = (props) => {
   const arr = [1, 2, 3, 4, 5, 6]
   return (
-    <div>
-      <h1 className="trending mb-3">Trending Topics</h1>
+    <div className="container-fluid border shadow">
+      <h1 className="trending pt-3">Trending Topics</h1>
       <Row>
         <div>
           {arr.map(item => {
@@ -21,7 +22,7 @@ const Trending = () => {
         </div>
       </Row>
 
-      <div class='container input-group my-5'>
+      <div class='container input-group pb-3 my-5'>
         <input
           type='text'
           class='form-control'
