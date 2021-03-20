021a36813ee73bc1a1a82151970e13c5f33ecb4e
Chris Burns <ckbzxz@gmail.com>
2021-03-20 16:15:20 -0400

    added news api functionality, requires local gitignored file with api keys

---
+++ client/src/testnewsdata.js
@@ -0,0 +1,43 @@
+const news = [
+    {
+    "source": {
+    "id": "cnn",
+    "name": "CNN"
+    },
+    "author": "Chuck Johnston, CNN",
+    "title": "GoFundMe page for sons of Atlanta spa shooting victim raises over $2 million - CNN",
+    "description": "A GoFundMe page for the two sons of Atlanta spa shooting victim Hyun Jung Grant had raised more than $2 million as of Saturday morning.",
+    "url": "https://www.cnn.com/2021/03/20/cnn10/hyun-jung-grant-atlanta-shooting-gofundme-trnd/index.html",
+    "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/210319025352-02-atlanta-shooting-memorial-0318-super-tease.jpg",
+    "publishedAt": "2021-03-20T16:03:00Z",
+    "content": "Watch \"AFRAID: Fear in Communities of Color\" at 9 pm ET on Monday, March 22 -- a CNN Special Report hosted by Amara Walker, Ana Cabrera, Victor Blackwell and Anderson Cooper.\r\n (CNN)A GoFundMe page f… [+2093 chars]"
+  },
+
+  {"source": {
+    "id": null,
+    "name": "TMZ"
+    },
+    "author": "TMZ Staff",
+    "title": "Chris Harrison Hires Powerful Lawyer Over 'Bachelor' Dispute - TMZ",
+    "description": "Chris Harrison knows the temperature in the room, because he's now lawyered up in the wake of his uncertain future as host of \"The Bachelor.\"",
+    "url": "https://www.tmz.com/2021/03/20/chris-harrison-hires-lawyer-the-bachelor-racism/",
+    "urlToImage": "https://imagez.tmz.com/image/1f/16by9/2021/03/20/1fc06fb09053497baa9d346a723327c3_xl.jpg",
+    "publishedAt": "2021-03-20T15:39:12Z",
+    "content": "Chris Harrison knows the temperature in the room, because he's now lawyered up in the wake of his uncertain future as host of \"The Bachelor.\"\r\nHarrison hired celeb attorney Bryan Freedman, who knows … [+860 chars]"
+  },
+  {
+    "source": {
+    "id": null,
+    "name": "ESPN"
+    },
+    "author": null,
+    "title": "Only 108 NCAA tournament brackets in ESPN's Tournament Challenge remain unblemished after first day - ESPN",
+    "description": "There were 14.7 million brackets entered in ESPN's Tournament Challenge prior to the start of the NCAA tournament. After just 16 games, only 108 remain unblemished.",
+    "url": "https://www.espn.com/mens-college-basketball/story/_/id/31099858/only-108-ncaa-tournament-brackets-espn-tournament-challenge-remain-unblemished-first-day",
+    "urlToImage": "https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2021%2F0320%2Fr829543_1296x729_16%2D9.jpg",
+    "publishedAt": "2021-03-20T15:31:23Z",
+    "content": "After a year without it, March Madness returned in a big way Friday with a No. 15 seed, a No. 13 seed, a No. 12 seed and a No. 11 seed all advancing to the second round of the NCAA tournament.\r\nAnd w… [+1761 chars]"
+  },
+]
+
+module.exports = news;
