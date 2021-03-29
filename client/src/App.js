278c4b2f9d35216204772439f9ce5a2a6c53da78
Torontow <mtorontow0@gmail.com>
2021-03-28 23:30:59 -0400

    Added footer component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,46 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Logo from './components/Logo'
-import Container from './components/Container'
-import Col from './components/Col'
-import Navbar from './components/Navbar'
-import Row from './components/Row'
-import ArticleDiscussionPage from './components/pages/ArticleDiscussionPage'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Logo from "./components/Logo";
+import Container from "./components/Container";
+import Col from "./components/Col";
+import Navbar from "./components/Navbar";
+import Row from "./components/Row";
+import Footer from "./components/Footer";
+import ArticleDiscussionPage from "./components/pages/ArticleDiscussionPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
-import Trending from "./components/Trending"
-import Landing from './components/Landing'
-import ProfileName from './components/ProfileName'
-import ProfilePic from './components/ProfilePic'
-import Profile from './components/Profile'
+import Trending from "./components/Trending";
+import Profile from "./components/Profile";
 
-function App () {
+function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Logo />
       <Router>
         <Container>
           <Row>
-            <Col size='1'>
+            <Col size="1">
               <Navbar />
             </Col>
-            <Col size='10'>
+            <Col size="10">
               <Route path="/login" component={Login} />
               <Route path="/sign-up" component={SignUp} />
-              <Route exact path='/' component={Trending} />
-              <Route exact path='/Discussion' component={ArticleDiscussionPage} />
-              <Route exact path='/profile' component={Profile} />
+              <Route exact path="/" component={Trending} />
+              <Route
+                exact
+                path="/Discussion"
+                component={ArticleDiscussionPage}
+              />
+              <Route exact path="/profile" component={Profile} />
             </Col>
           </Row>
         </Container>
       </Router>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
