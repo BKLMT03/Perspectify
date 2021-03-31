4b29242d3127810bc07cd6f812d4f870df0b6705
Torontow <mtorontow0@gmail.com>
2021-03-31 16:19:06 -0400

    Made sign-up linked.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import Typed from 'react-typed'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button'
-import './style.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Typed from "react-typed";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import "./style.css";
+import SignUp from "../pages/SignUp";
 
-function Header () {
+function Header() {
   return (
-    <div className='container text-center header mb-5'>
+    <div className="container text-center header mb-5">
       <Row>
-        <h3 className='subtitle mt-4 mb-5'>
+        <h3 className="subtitle mt-4 mb-5">
           <Typed
-            strings={['Earn Your Opinion.', 'Escape the echo chamber.' , 'Challenge your beliefs.', 'PERSPECTIFY yourself.']}
+            strings={[
+              "Earn Your Opinion.",
+              "Escape the echo chamber.",
+              "Challenge your beliefs.",
+              "Think before you speak.",
+              "PERSPECTIFY yourself.",
+            ]}
             startDelay={100}
             typeSpeed={80}
             backSpeed={40}
@@ -20,17 +28,19 @@ function Header () {
         </h3>
       </Row>
       <Row>
+        <p></p>
         <p>
-           
+          Easily find the latest news on trending topics from a variety of news
+          sources and points of view all in one place.
         </p>
+
         <p>
-          Easily find the latest news on trending topics from a variety of news sources
-          and points of view all in one place.
+          <Link to="/sign-up">Sign up</Link> to discuss with other PERSPECTIFIED
+          minds.
         </p>
-        <p>Sign up to discuss with other PERSPECTIFIED minds.</p>
       </Row>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
