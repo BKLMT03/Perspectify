cd9ad4f2f9dc731b1feaaf367f5c4cef4d604e3c
Chris Burns <ckbzxz@gmail.com>
2021-04-01 00:32:11 -0400

    back end setup for authentication using jwt

--- models/User.js
+++ models/User.js
@@ -14,12 +14,17 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
+        unique: true,
         required: true
     },
     password: {
         type: String,
         trim: true,
         required: true
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
     },
     image: {
         type: String,
--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getComments, addComment} = require('../controllers/comments')
+const auth = require('../middleware/auth')
 
 router.route('/')
-    .get(getComments)
-    .post(addComment)
+    .get(auth, getComments)
+    .post(auth, addComment)
 
 module.exports = router;
